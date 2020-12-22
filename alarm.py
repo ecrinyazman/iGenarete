22a69b00bf57e9a1cf42bd171547855f639f887e
ecrinyazman <75580740+ecrinyazman@users.noreply.github.com>
2020-12-22 05:27:43 +0300

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,8 @@
+saat=int(input("Lüften saati giriniz"))
+  if saat<7 or saat>7: 
+    print("Uyumaya devam.") 
+  elif saat==7: 
+    print("Alarm çalıyor, kalk!") 
+  else: 
+    print("Lütfen bir saat giriniz.") 
+view raw
