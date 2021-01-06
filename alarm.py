45a659c02bb5540cf392516d14b537cf5ca22b82
ecrinyazman <75580740+ecrinyazman@users.noreply.github.com>
2021-01-06 12:39:33 +0300

    Delete alarm.py

--- alarm.py
+++
@@ -1,7 +0,0 @@
-saat=int(input("Lüften saati giriniz"))
-  if saat<7 or saat>7: 
-    print("Uyumaya devam.") 
-  elif saat==7: 
-    print("Alarm çalıyor, kalk!") 
-  else: 
-    print("Lütfen bir saat giriniz.") 
